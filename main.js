2cb0a8251a34de97397a9190e655df43826c2764
Leonardo Bellettini <leobel96@live.it>
2019-12-11 21:21:23 +0100

    First Upload

---
+++ main.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+  new Vue({
+    el: '#app',
+    data: {
+      page: "home",
+      oldY: -1,
+      maxScroll: 0
+    },
+    methods: {
+      changeBack: function () {
+        var Y = window.scrollY;
+        if (this.oldY != Y) {
+          this.oldY = Y;
+          if (this.maxScroll == Infinity) {
+            this.maxScroll = 0;
+          }
+          var hue = (Y * this.maxScroll + 185) | 0;
+          var hsl = "hsl(" + hue + ", 93%, 51%)";
+          document.body.style.backgroundColor = hsl;
+        }
+      }
+    },
+    mounted: function () {
+      window.addEventListener("scroll", () => {
+        this.changeBack();
+      });
+    },
+    updated: function () {
+      this.maxScroll = 50 / window.scrollMaxY;
+    }
+  })
+}
+
+// 185 - 235
